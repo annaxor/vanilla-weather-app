6dde6db1db2286812c690fde280be79683665e99
annaxor <roxannaortega@gmail.com>
2021-11-10 20:31:02 +0000

    updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+function formatdate(timestamp) {
+    let date = new Date (timestamp);
+    let hour = date.getHours();
+    if (hour < 10) {
+        hour = `0${hour}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[date.getDay()];
+
+     return `${day} ${hour}:${minutes}`
+}
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature")
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -14,7 +30,8 @@ function displayTemperature(response) {
     let windElement = document.querySelector("#wind")
     windElement.innerHTML = Math.round(response.data.wind.speed);
 
-    
+    let dateElement = document.querySelector("#date")
+    dateElement.innerHTML = formatdate(response.data.dt * 1000);    
 }
 
 let apiKey = "251603b3b44255d6a7ae3a0215435b62";
