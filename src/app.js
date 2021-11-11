c893240a1c312ddd75814c17c20b5c18d9761bb3
annaxor <roxannaortega@gmail.com>
2021-11-11 20:48:57 +0000

    added a form

--- src/app.js
+++ src/app.js
@@ -38,12 +38,23 @@ function displayTemperature(response) {
     let iconElement = document.querySelector("#icon")
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
-
-
 }
 
-let city = "New York";
-let apiKey = "251603b3b44255d6a7ae3a0215435b62";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+//search engine
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city){
+    let apiKey = "251603b3b44255d6a7ae3a0215435b62";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
+
+search("New York")
+
+function handleSubmit (event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
